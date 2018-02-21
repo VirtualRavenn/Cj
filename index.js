d33ce4c630cb8e609888614dc57ce519a2c7923c
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-21 17:02:15 +1100

    Update index.js

--- index.js
+++ index.js
@@ -11,4 +11,23 @@ client.on('message', msg => {
   }
 });
 
+if(command === "purge") {
+        let messagecount = parseInt(args[1]) || 1;
+
+        var deletedMessages = -1;
+
+        message.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
+            messages.forEach(m => {
+                if (m.author.id == bot.user.id) {
+                    m.delete().catch(console.error);
+                    deletedMessages++;
+                }
+            });
+        }).then(() => {
+                if (deletedMessages === -1) deletedMessages = 0;
+                message.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
+                    .then(m => m.delete(2000));
+        }).catch(console.error);
+    }
+
 client.login(process.env.BOT_TOKEN);
