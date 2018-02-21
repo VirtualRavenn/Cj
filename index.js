e7dcb340b32e86fe77dd8d8e9665c586b33d1225
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-21 15:06:08 +1100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 const client = new Discord.Client();
 
 client.on("message", message => {
-  if (message.author.id === "415330227451985920") message.reply("**Fool** you aren't suppose to be in here.");
+  if (message.author.id === "415704844221808640") message.reply("**Fool** you aren't suppose to be in here.");
 });
 
 client.on('message', msg => {
@@ -10,5 +10,5 @@ client.on('message', msg => {
     msg.reply("I'll have 2 number nines, a number nine large, a number six with extra dip, a number seven, 2 number 45's, one with cheese and a large soda.");
   }
 });
-                 
+
 client.login(process.env.BOT_TOKEN);
