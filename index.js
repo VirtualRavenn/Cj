703e716e8e45f7ea2b132331af9c4e8a7aa66132
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-21 14:39:56 +1100

    Update index.js

--- index.js
+++ index.js
@@ -4,5 +4,11 @@ const client = new Discord.Client();
 client.on("message", message => {
   if (message.author.id === "292545042059624449") message.reply("**Fool** you ank suppose to be in here.");
 });
+
+client.on('message', msg => {
+  if (msg.content.toLowerCase()=== "@Cj#7244") {
+    msg.reply("Shut the fuck up, Bitch!");
+  }
+});
                  
 client.login(process.env.BOT_TOKEN);
