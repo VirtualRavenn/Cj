f064c8a830c06d48bafa93521199d343f0191d20
VirtualRavenn <32814604+VirtualRavenn@users.noreply.github.com>
2018-02-22 13:30:53 +1100

    Update index.js

--- index.js
+++ index.js
@@ -9,4 +9,10 @@ client.on("message", message => {
   if (message.author.id === "415996653279903744") message.reply("**Fool** you aren't suppose to be in here.");
 });
 
+client.on('message', msg => {
+  if (msg.content.toLowerCase()=== "") {
+    msg.reply("");
+  }
+});
+
 client.login(process.env.BOT_TOKEN);
